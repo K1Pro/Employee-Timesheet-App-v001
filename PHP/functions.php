6e2c209e61eee5dd6212fe7e85ebbb59c755fc3c
K1Pro <support@k1pro.net>
2022-11-16 20:16:48 +0100

    migrated to PHP 8 implemented str_contains.working

--- PHP/functions.php
+++ PHP/functions.php
@@ -4,22 +4,27 @@ function listFirstLogonNodeActivity($calendarDate, $nodeList) {
   $scanned_directory = array_diff(scandir($directory), array('..', '.'));
   $uniqueNodeList =[];
   foreach( $scanned_directory as $logFiles ) {
-    $OSAction = substr($logFiles, -12, 8);
-    $scannedNode = substr($logFiles, 0, 26);
-    if (in_array($scannedNode, $nodeList) && !in_array($scannedNode, $uniqueNodeList)) {
-      if ($OSAction == "Logon_at") {
-          $timeOfScannedNode = substr($logFiles, 27, 5);
-          echo '<div class="text-light border-bottom border-white nodeActivity ';
-            if (substr($scannedNode, 0, 8) == "Bartosz_") {echo 'BartoszTime';}
-            if (substr($scannedNode, 0, 8) == "Hanna___") {echo 'HannaTime';}
-            if (substr($scannedNode, 0, 8) == "Joanna__") {echo 'JoannaTime';}
-          echo '">' . str_replace("_", ":", $timeOfScannedNode) . ' ';
-          echo str_replace("_", "", substr($scannedNode, 9, 17)) . '</div>' . "\n";
-          if(!in_array($scannedNode, $uniqueNodeList , true)){
-            array_push($uniqueNodeList , $scannedNode);
-        }
-        }
-      }
+    if (str_contains($logFiles, 'TurnedOn')) {
+      echo $logFiles . "\n";
+  }
+
+    // echo $logFiles;
+    // $OSAction = substr($logFiles, -12, 8);
+    // $scannedNode = substr($logFiles, 0, 26);
+    // if (in_array($scannedNode, $nodeList) && !in_array($scannedNode, $uniqueNodeList)) {
+    //   if ($OSAction == "Logon_at") {
+    //       $timeOfScannedNode = substr($logFiles, 27, 5);
+    //       echo '<div class="text-light border-bottom border-white nodeActivity ';
+    //         if (substr($scannedNode, 0, 8) == "Bartosz_") {echo 'BartoszTime';}
+    //         if (substr($scannedNode, 0, 8) == "Hanna___") {echo 'HannaTime';}
+    //         if (substr($scannedNode, 0, 8) == "Joanna__") {echo 'JoannaTime';}
+    //       echo '">' . str_replace("_", ":", $timeOfScannedNode) . ' ';
+    //       echo str_replace("_", "", substr($scannedNode, 9, 17)) . '</div>' . "\n";
+    //       if(!in_array($scannedNode, $uniqueNodeList , true)){
+    //         array_push($uniqueNodeList , $scannedNode);
+    //     }
+    //     }
+    //   }
   }
 }
 // Side Panel
